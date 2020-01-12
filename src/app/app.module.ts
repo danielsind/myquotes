d21089f8b5fb829c437a40ea58887a5bf3fd1a41
danielsind <danielsindet@gmail.com>
2020-01-12 11:04:41 +0300

    Create components quote-detail.component.html and quote-detail.component.ts --Export and import QuoteDetailComponent--Declared QuoteDetailComponent at app.module.ts @NgModule declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { CreateComponent } from './createQuote/create.component';
+import { QuoteDetailComponent } from './quote/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteComponent,
     CreateComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quote/quote-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector:'quote-detail',
+    template:'./quote-detail.component.html'
+})
+export class QuoteDetailComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
